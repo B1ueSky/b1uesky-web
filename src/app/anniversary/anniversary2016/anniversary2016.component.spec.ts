3ff7103320dc4839ba72beefc28f39f776ceeb66
B1ueSky <programmer.lty@gmail.com>
2017-01-14 02:50:01 -0500

    add anniversary2016 component

---
+++ src/app/anniversary/anniversary2016/anniversary2016.component.spec.ts
@@ -0,0 +1,28 @@
+/* tslint:disable:no-unused-variable */
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+
+import { Anniversary2016Component } from './anniversary2016.component';
+
+describe('Anniversary2016Component', () => {
+  let component: Anniversary2016Component;
+  let fixture: ComponentFixture<Anniversary2016Component>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ Anniversary2016Component ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(Anniversary2016Component);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/anniversary/anniversary2016/anniversary2016.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-anniversary2016',
+  templateUrl: './anniversary2016.component.html',
+  styleUrls: ['./anniversary2016.component.sass']
+})
+export class Anniversary2016Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
