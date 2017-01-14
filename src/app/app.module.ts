52e4ec44c9d6db290e5b70ec407d705ccabd087f
B1ueSky <programmer.lty@gmail.com>
2017-01-14 02:47:09 -0500

    add page-not-found component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,12 @@ import {HttpModule} from '@angular/http';
 import {AppComponent} from './app.component';
 import {AppRouter} from "./app.router";
 import {AnniversaryModule} from "./anniversary/anniversary.module";
+import {PageNotFoundComponent} from "./page-not-found/page-not-found.component";
 
 @NgModule({
     declarations: [
-        AppComponent
+        AppComponent,
+        PageNotFoundComponent
     ],
     imports: [
         AnniversaryModule,
