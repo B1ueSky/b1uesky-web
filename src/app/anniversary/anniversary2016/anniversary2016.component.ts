5e5c6bb56eee4a7c5cc6ebae4b98422d6779e7a7
B1ueSky <programmer.lty@gmail.com>
2017-01-15 03:31:05 -0500

    change sass to scss.

--- src/app/anniversary/anniversary2016/anniversary2016.component.ts
+++ src/app/anniversary/anniversary2016/anniversary2016.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import * as _ from 'lodash';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
+import {Subscription} from "rxjs";
 
 @Component({
-  selector: 'app-anniversary2016',
-  templateUrl: './anniversary2016.component.html',
-  styleUrls: ['./anniversary2016.component.sass']
+    selector: 'app-anniversary2016',
+    templateUrl: './anniversary2016.component.html',
+    styleUrls: ['./anniversary2016.component.scss']
 })
 export class Anniversary2016Component implements OnInit {
+    private season: string;
+    private subscription: Subscription;
 
-  constructor() { }
+    constructor(private route: ActivatedRoute) {
+    }
 
-  ngOnInit() {
-  }
+    public ngOnInit(): void {
+        this.subscription = this.route.queryParams.subscribe((params: any) => {
+            this.season = _.indexOf(['spring', 'summer', 'autumn', 'winter'], params.season) > -1 ?
+                            params.season : null;
+        });
+    }
+
+    public ngOnDestroy(): void {
+        // prevent memory leak
+        this.subscription.unsubscribe();
+    }
 
 }
