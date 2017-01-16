3a7b289be2c2f7bea8d4c2b5f82fb8353fc2c2e3
B1ueSky <programmer.lty@gmail.com>
2017-01-16 01:40:05 -0500

    create image-wall component

--- src/app/anniversary/anniversary.module.ts
+++ src/app/anniversary/anniversary.module.ts
@@ -2,13 +2,14 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import { Anniversary2016Component } from './anniversary2016/anniversary2016.component';
 import {AnniversaryRouter} from "./anniversary.router";
+import { ImageWallComponent } from './directives/image-wall/image-wall.component';
 
 @NgModule({
     imports: [
         AnniversaryRouter,
         CommonModule
     ],
-    declarations: [Anniversary2016Component]
+    declarations: [Anniversary2016Component, ImageWallComponent]
 })
 export class AnniversaryModule {
 }
