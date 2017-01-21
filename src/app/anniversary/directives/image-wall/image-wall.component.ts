ed3263428795c5d9950a7a8f133097b0a8ebfdcb
B1ueSky <programmer.lty@gmail.com>
2017-01-21 01:33:10 -0500

    rotate images

--- src/app/anniversary/directives/image-wall/image-wall.component.ts
+++ src/app/anniversary/directives/image-wall/image-wall.component.ts
@@ -1,3 +1,4 @@
+import * as _ from 'lodash';
 import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
@@ -8,6 +9,7 @@ import {Component, Input, OnInit} from '@angular/core';
 export class ImageWallComponent implements OnInit {
     @Input() private season: string;
     @Input() private images: string[][];
+    private allImages: string[];
 
     constructor() {
     }
@@ -17,13 +19,13 @@ export class ImageWallComponent implements OnInit {
             [
                 'http://www.newsofbahrain.com/admin/post/upload/000PST_31-03-2016_1459426231_bYViJTGH2j.jpg',
                 'http://www.hdwallpaperspop.com/wp-content/uploads/2016/09/beautiful-girl-hd-wallpaper-for-walls.jpg',
-                'http://www.shunvmall.com/data/out/23/46993889-beautiful-girl-wallpaper.jpg',
+                'http://images6.fanpop.com/image/photos/37600000/The-most-beautiful-girl-at-world-beautiful-pictures-37694196-375-500.jpg',
                 'http://onehdwallpaper.com/wp-content/uploads/2016/11/Beautiful-Cute-Girl-Smile-HD-Wallpaper.jpg',
-                'http://www.shunvmall.com/data/out/23/46916268-beautiful-girl.jpg',
+                'https://driving-art.info/uploads/23/35948090-beautiful-girl-image.jpg',
             ],
             [
                 'http://www.wonderslist.com/wp-content/uploads/2015/10/Doutzen-Kroes-Most-Beautiful-Dutch-Woman.jpg',
-                'http://7-themes.com/data_images/collection/2/4441224-beautiful-girl-wallpapers.jpg',
+                'http://www.qygjxz.com/data/out/124/3898670-images-of-beautiful-girl.jpg',
                 'http://onehdwallpaper.com/wp-content/uploads/2016/11/Beautiful-Girl-Wallpaper-in-FUll-HD.jpg',
                 'http://onehdwallpaper.com/wp-content/uploads/2016/11/World-Beautiful-Girls-Images-Free-Download.jpg',
                 'http://hdpic.org/wp-content/uploads/2015/02/beautiful-girl-Fascinating-Wallpaper-Exquisite-Free-Picture.jpg',
@@ -32,13 +34,13 @@ export class ImageWallComponent implements OnInit {
                 'http://www.ultrahighdefinitionwallpapers.com/wp-content/uploads/2015/04/beautiful-girl-uhd-wallpapers.jpg',
                 'http://eskipaper.com/images/beautiful-girls-18.jpg',
                 'http://www.newsread.in/wp-content/uploads/2016/06/Most-Beautiful-Girl-In-The-World-9.jpg',
-                'http://eskipaper.com/images/beautiful-girl-mood-wallpaper-2.jpg',
+                'https://s-media-cache-ak0.pinimg.com/736x/6f/90/73/6f907335b64627973370548e9d2ef00e.jpg',
                 'http://www.wallpaperscharlie.com/wp-content/uploads/2016/09/Beautiful-Girl-Image-3.jpg',
             ],
             [
                 'http://onehdwallpaper.com/wp-content/uploads/2015/11/Most-Beautiful-Girl-in-Flowers-Field.jpg',
-                'http://7-themes.com/data_images/out/55/6959032-high-resolution-wallpaper-beautiful-girl.jpg',
-                'http://7-themes.com/data_images/out/72/7019491-beautiful-girl.jpg',
+                'http://www.lovethispic.com/uploaded_images/34085-Beautiful-Girl.jpg',
+                'http://eskipaper.com/images/beautiful-girl-mood-wallpaper-2.jpg',
                 'http://eskipaper.com/images/beautiful-girls-30.jpg',
                 'http://www.wonderslist.com/wp-content/uploads/2015/04/Deepika-Padukone-Beautiful-Women-of-2015.jpg',
             ],
@@ -50,6 +52,16 @@ export class ImageWallComponent implements OnInit {
                 'http://dawallpaper.com/wp-content/uploads/2015/01/school-beautiful-girl-800x600.jpg'
             ],
         ];
+        this.allImages = _.flattenDeep(this.images) as string[];
     }
 
+    public styleImage() {
+        const angle = _.random(-5, 5);
+        return {
+            'transform': 'translate(-50%, -50%) rotate(' + angle + 'deg) ',
+            '-webkit-transform': 'translate(-50%, -50%) rotate(' + angle + 'deg) ',
+            'left': '50%',
+            'top': '50%',
+        };
+    }
 }
