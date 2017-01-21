f650695bf181bf041786253ae888533e885f4662
B1ueSky <programmer.lty@gmail.com>
2017-01-21 02:39:06 -0500

    add fx-layout to anniversary module

--- src/app/anniversary/anniversary.module.ts
+++ src/app/anniversary/anniversary.module.ts
@@ -1,14 +1,16 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
-import { Anniversary2016Component } from './anniversary2016/anniversary2016.component';
+import {Anniversary2016Component} from './anniversary2016/anniversary2016.component';
 import {AnniversaryRouter} from "./anniversary.router";
-import { ImageWallComponent } from './directives/image-wall/image-wall.component';
+import {ImageWallComponent} from './directives/image-wall/image-wall.component';
 import {MaterialModule} from "@angular/material";
+import {FlexLayoutModule} from '@angular/flex-layout';
 
 @NgModule({
     imports: [
         AnniversaryRouter,
         CommonModule,
+        FlexLayoutModule.forRoot(),
         MaterialModule.forRoot(),
     ],
     declarations: [Anniversary2016Component, ImageWallComponent]
