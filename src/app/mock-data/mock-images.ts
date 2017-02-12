3ca8314f577d69b8e0c3d21ec79144e07c0b470b
B1ueSky <programmer.lty@gmail.com>
2017-02-12 03:12:56 -0500

    add thumbnail property to image model

--- src/app/mock-data/mock-images.ts
+++ src/app/mock-data/mock-images.ts
@@ -7,31 +7,36 @@ export const mockImages: {[key: string]: Image[]} = {
     winter: _.map(_.range(1, 36), (num): Image => {
         return {
             _id: num.toString(),
-            path: resourceBase + 'anniversary/' + 'autumn - ' + num + '.jpg'
+            path: resourceBase + 'anniversary/' + 'autumn - ' + num + '.jpg',
+            thumbnail: resourceBase + 'anniversary/' + 'autumn - ' + num + '_tn.jpg'
         };
     }),
     spring: _.map(_.range(1, 36), (num): Image => {
         return {
             _id: num.toString(),
-            path: resourceBase + 'anniversary/' + 'autumn - ' + num + '.jpg'
+            path: resourceBase + 'anniversary/' + 'autumn - ' + num + '.jpg',
+            thumbnail: resourceBase + 'anniversary/' + 'autumn - ' + num + '_tn.jpg'
         };
     }),
     summer: _.map(_.range(1, 36), (num): Image => {
         return {
             _id: num.toString(),
-            path: resourceBase + 'anniversary/' + 'autumn - ' + num + '.jpg'
+            path: resourceBase + 'anniversary/' + 'autumn - ' + num + '.jpg',
+            thumbnail: resourceBase + 'anniversary/' + 'autumn - ' + num + '_tn.jpg'
         };
     }),
     autumn: _.map(_.range(1, 36), (num): Image => {
         return {
             _id: num.toString(),
-            path: resourceBase + 'anniversary/' + 'autumn - ' + num + '.jpg'
+            path: resourceBase + 'anniversary/' + 'autumn - ' + num + '.jpg',
+            thumbnail: resourceBase + 'anniversary/' + 'autumn - ' + num + '_tn.jpg'
         };
     }),
     puzzle: _.map(_.range(1, 9), (num): Image => {
         return {
             _id: num.toString(),
-            path: resourceBase + 'anniversary/' + 'autumn - ' + num + '.jpg'
+            path: resourceBase + 'anniversary/' + 'autumn - ' + num + '.jpg',
+            thumbnail: resourceBase + 'anniversary/' + 'autumn - ' + num + '_tn.jpg'
         };
     })
 };
--- src/app/models/image.ts
+++ src/app/models/image.ts
@@ -1,11 +1,13 @@
 export class Image {
-    public _id     : string;
-    public path    : string;
-    public comment?: string;
+    public _id        : string;
+    public comment?   : string;
+    public path       : string;
+    public thumbnail? : string;
 
     constructor(obj: any) {
-        this._id     = obj._id;
-        this.path    = obj.path;
-        this.comment = obj.comment;
+        this._id       = obj._id;
+        this.comment   = obj.comment;
+        this.path      = obj.path;
+        this.thumbnail = obj.thumbnail;
     }
 }
