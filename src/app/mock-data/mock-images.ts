e22e19633ca100d7481f0665bcbb89eb6575431b
B1ueSky <programmer.lty@gmail.com>
2017-02-12 03:19:48 -0500

    use thumbnails.... now the image wall can display as many images as I want!

--- src/app/mock-data/mock-images.ts
+++ src/app/mock-data/mock-images.ts
@@ -4,28 +4,28 @@ import {Image} from "../models/image";
 const resourceBase = '../assets/images/';
 
 export const mockImages: {[key: string]: Image[]} = {
-    winter: _.map(_.range(1, 36), (num): Image => {
+    winter: _.map(_.range(1, 114), (num): Image => {
         return {
             _id: num.toString(),
             path: resourceBase + 'anniversary/' + 'autumn - ' + num + '.jpg',
             thumbnail: resourceBase + 'anniversary/' + 'autumn - ' + num + '_tn.jpg'
         };
     }),
-    spring: _.map(_.range(1, 36), (num): Image => {
+    spring: _.map(_.range(1, 114), (num): Image => {
         return {
             _id: num.toString(),
             path: resourceBase + 'anniversary/' + 'autumn - ' + num + '.jpg',
             thumbnail: resourceBase + 'anniversary/' + 'autumn - ' + num + '_tn.jpg'
         };
     }),
-    summer: _.map(_.range(1, 36), (num): Image => {
+    summer: _.map(_.range(1, 114), (num): Image => {
         return {
             _id: num.toString(),
             path: resourceBase + 'anniversary/' + 'autumn - ' + num + '.jpg',
             thumbnail: resourceBase + 'anniversary/' + 'autumn - ' + num + '_tn.jpg'
         };
     }),
-    autumn: _.map(_.range(1, 36), (num): Image => {
+    autumn: _.map(_.range(1, 114), (num): Image => {
         return {
             _id: num.toString(),
             path: resourceBase + 'anniversary/' + 'autumn - ' + num + '.jpg',
