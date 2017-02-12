61fb3460f1691d892cc27ae367a25e1b9fe53289
B1ueSky <programmer.lty@gmail.com>
2017-02-12 02:18:54 -0500

    styling for season selection tabs

--- src/app/mock-data/mock-images.ts
+++ src/app/mock-data/mock-images.ts
@@ -4,25 +4,25 @@ import {Image} from "../models/image";
 const resourceBase = '../assets/images/';
 
 export const mockImages: {[key: string]: Image[]} = {
-    winter: _.map(_.range(1, 114), (num): Image => {
+    winter: _.map(_.range(1, 36), (num): Image => {
         return {
             _id: num.toString(),
             path: resourceBase + 'anniversary/' + 'autumn - ' + num + '.jpg'
         };
     }),
-    spring: _.map(_.range(1, 114), (num): Image => {
+    spring: _.map(_.range(1, 36), (num): Image => {
         return {
             _id: num.toString(),
             path: resourceBase + 'anniversary/' + 'autumn - ' + num + '.jpg'
         };
     }),
-    summer: _.map(_.range(1, 114), (num): Image => {
+    summer: _.map(_.range(1, 36), (num): Image => {
         return {
             _id: num.toString(),
             path: resourceBase + 'anniversary/' + 'autumn - ' + num + '.jpg'
         };
     }),
-    autumn: _.map(_.range(1, 114), (num): Image => {
+    autumn: _.map(_.range(1, 36), (num): Image => {
         return {
             _id: num.toString(),
             path: resourceBase + 'anniversary/' + 'autumn - ' + num + '.jpg'
